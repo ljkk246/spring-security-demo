de66bd1a18501be1ba8a395c4dca59057cca9269
ljkk246 <ljkk246@gmail.com>
2021-08-23 23:03:54 +0900

    step01 : 08 : AnnoymousAuthenticationFilter 설명

--- src/main/java/com/example/springsecuritydemo/SecurityConfiguration.java
+++ src/main/java/com/example/springsecuritydemo/SecurityConfiguration.java
@@ -29,37 +29,25 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
         http.formLogin();
 
-        http.logout() //로그아웃처리. POST 방식. (GET은 커스터마이징 필요)
-                .logoutUrl("/logout")//로그 아웃 처리 url
-                .logoutSuccessUrl("/login")//로그아웃 성공 후 이동페이지
-                .deleteCookies("JSESSIONID", "remember-me")//로그아웃 후 쿠키들 삭제
-                .addLogoutHandler(new LogoutHandler() {
-                    @Override
-                    public void logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
-                        HttpSession session = request.getSession();
-                        session.invalidate();//세션 무효화
-                    }
-                })//로그아웃 핸들러
-                .logoutSuccessHandler(new LogoutSuccessHandler() {
-                    @Override
-                    public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
-                        response.sendRedirect("/login");//로그아웃 성공 후 리다이렉트
-                    }
-                });//로그아웃 성공 후 핸들러
         http.rememberMe()
-                .rememberMeParameter("remember")//기본 파라미터명은 remember-me
-                .tokenValiditySeconds(3600)//리멤버 미 쿠키 만료 시간. 기본값은 14일
-                .alwaysRemember(true)//리멤버미 기능이 활성화되지 않아도 항상 실행
-                .userDetailsService(userDetailsService);//리멤버 미를 쿠키를 인증할때 사용자를 조회 해야 하는데 그 때 필요한 서비스.
-        // * remember-me 설명 *
-        //인증이되었다는 건 그 사용자가 세션이 생성되었다느 것. 그리고 그 세션이 성공된 인증 객체를 담고 있음.
-        //서버 같은 경우 인증에 성공한 그 사용자에게 세션을 생성할 때 가지고 있는 jsessionid를 응답에 실어서 보냄. 클라이언트는 jsessionid를 가지고 있음.
-        //클라이언트는 다시 서버에 접속할 때 인증이 필요 없는데 그 이유는
-        //클라이언트가 다시 세션에 접속할 때 서버는 세션id를 가지고 가서 매칭되는 세션을 꺼내서 인증된 사용자인지 판단해서 처리하게 됨.
+                .userDetailsService(userDetailsService);
+        //AnonymousAuthenticationFilter
+        //지금까지는 user객체가 null이면 인증을 받지 않았다고 판단하고 자원에 접근하지 못하도록 막음.
+        //이 개념이 AnonymousAuthenticationFilter에서도 동일하지만 AnonymousAuthenticationFilter에서는 null로 처리하지 않고 별도의 익명 사용자로 만듬.
+        //user 객체가 null이면 익명 사용자용 인증객체를 만든다는게 차이점.
+        //SecurityContextHoler에 익명사용자 토큰객체를 저장함.
 
-        //그런데, editTHisCookie 크롬 툴로 JSESSIONID를 삭제하면, 서버에서는 세션을 못받아서 처음 접속하는 사용자인줄 알고 로그인페이지로 이동하게 됨.
+        //익명사용자 필터는 언제 사용?
+        //isAnonymous()-> true이면 로그인 버튼을 보여줌. isAuthenticated() -> true이면 로그아웃 버튼을 보여줌.
 
-        //이때 remember-me 쿠키를 체크하고 로그인 후 JSESSIONID를 다시 삭제하고 새로고침해보면, 이떄는 로그인페이지로 이동하지 않는다
-        //그 이유는 JSESSIONID가 없다고 하더라도 remember-me 라는 쿠키의 값을 디코드,파싱해서 유저 계정을 얻어서 다시 인증을 시도하기 떄문이다.
+        //익명사용자 필터는 인증 객체를 세션에 저장하지 않는다.
+
+        //AbstractSecurityInterceptor는 필터중 맨 마지막 단계에서 사용자에게 자원을 허용/비허용하는 '인가'처리를 담당한다.
+
+
+
+
+
+
     }
 }
