8e6e06eded8f6548b572adedddd5b5ad07998c6f
ljkk246 <ljkk246@gmail.com>
2021-08-24 20:30:54 +0900

    step01 : 09 : 동시 세션 제어 설정해보기

--- src/main/java/com/example/springsecuritydemo/SecurityConfiguration.java
+++ src/main/java/com/example/springsecuritydemo/SecurityConfiguration.java
@@ -5,16 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.web.authentication.logout.LogoutHandler;
-import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-import java.io.IOException;
 
 @Configuration
 @EnableWebSecurity
@@ -31,22 +22,12 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
         http.rememberMe()
                 .userDetailsService(userDetailsService);
-        //AnonymousAuthenticationFilter
-        //지금까지는 user객체가 null이면 인증을 받지 않았다고 판단하고 자원에 접근하지 못하도록 막음.
-        //이 개념이 AnonymousAuthenticationFilter에서도 동일하지만 AnonymousAuthenticationFilter에서는 null로 처리하지 않고 별도의 익명 사용자로 만듬.
-        //user 객체가 null이면 익명 사용자용 인증객체를 만든다는게 차이점.
-        //SecurityContextHoler에 익명사용자 토큰객체를 저장함.
-
-        //익명사용자 필터는 언제 사용?
-        //isAnonymous()-> true이면 로그인 버튼을 보여줌. isAuthenticated() -> true이면 로그아웃 버튼을 보여줌.
-
-        //익명사용자 필터는 인증 객체를 세션에 저장하지 않는다.
-
-        //AbstractSecurityInterceptor는 필터중 맨 마지막 단계에서 사용자에게 자원을 허용/비허용하는 '인가'처리를 담당한다.
-
-
-
-
+        //동시 세션 제어
+        http.sessionManagement()//세션 관리 기능이 작동함.
+                .maximumSessions(1)//최대 허용 가능 세션수. -1 : 무제한 로그인 세션 허용
+                .maxSessionsPreventsLogin(false) //동시 로그인 차단함. -> 현재 사용가 차단. 기본값은 false -> 기존 세션 만료
+                .expiredUrl("/expired")//세션이 만료될 경우 이동할 페이지
+        ;
 
 
     }
