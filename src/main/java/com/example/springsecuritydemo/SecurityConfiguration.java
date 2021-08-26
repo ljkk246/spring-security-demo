63c4d976e9565afa3a88e54d8193cac138b664d4
ljkk246 <ljkk246@gmail.com>
2021-08-26 23:33:55 +0900

    step01 : 11 : 인가처리와 표현식 적용해보기

--- src/main/java/com/example/springsecuritydemo/SecurityConfiguration.java
+++ src/main/java/com/example/springsecuritydemo/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package com.example.springsecuritydemo;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -14,23 +15,28 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Autowired
     UserDetailsService userDetailsService;
 
+    //사용자 생성.
+    //noop prefix는 패스워드 암호화 방식을 나타냄.
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("user").password("{noop}1111").roles("USER");
+        auth.inMemoryAuthentication().withUser("sys").password("{noop}1111").roles("SYS", "USER");
+        auth.inMemoryAuthentication().withUser("admin").password("{noop}1111").roles("ADMIN", "SYS", "USER");
+        //지금은 ADMIN, SYS, USER 일일이 지정해야하지만 나중에 ADMIN 권한은 ADMIN만 넣으면 되게끔 하는 방법 배움.
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        //인가API - 권한설정
+        //특정한 자원에 대해 보안 검사를 하고 싶을때 antMatchers()를 사용한다.
+        //단 구체적인 경로가 먼저오고 그것보다 큰 범위의 경로가 뒤에 오도록 해야 한다.
         http.authorizeRequests()
+                .antMatchers("/user").hasRole("USER")
+                .antMatchers("/admin/pay").hasRole("ADMIN")
+                .antMatchers("/admin/**").access("hasRole('ADMIN') or hasRole('SYS')")
                 .anyRequest().authenticated();
 
         http.formLogin();
-
-        http.rememberMe()
-                .userDetailsService(userDetailsService);
-        http.sessionManagement()
-                .maximumSessions(1)
-                .maxSessionsPreventsLogin(true);
-        //세션 제어 필터 : SessionManagementFilter, ConcurrentSessionFilter
-        //SessionManagementFilter 는 세션 관리에 대한 filter이고 ConcurrentSessionFilter는 동시 세션 제어에 대한 filter다.
-        //SessionManagementFilter 의 ConcurrentSessionControlAuthenticationStrategy : 세션이 이미 존재하는지 체크 함.
-        //SessionManagementFilter 의 ChangeSessionIdAuthenticationStrategy : 세션 고정 보호처리를 함. 세션Id를 새로 만듬.
-        //SessionManagementFilter 의 RegisterSessionAuthenticationStrategy : 사용자의 세션을 등록하고 저장함.
-        //ConcurrentSessionFilter : session.isExpired -> ConcurrentSessionControlAuthenticationStrategy 호출 하여 세션 만료 체크함.
     }
+
 }
