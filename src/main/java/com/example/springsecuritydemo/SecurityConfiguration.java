f0e87e398abca9be8391a8668e7e8b7a00cec548
ljkk246 <ljkk246@gmail.com>
2021-08-27 22:51:14 +0900

    step01 : 12 : 인증/인가 예외 발생 처리 알아보기

--- src/main/java/com/example/springsecuritydemo/SecurityConfiguration.java
+++ src/main/java/com/example/springsecuritydemo/SecurityConfiguration.java
@@ -2,12 +2,26 @@ package com.example.springsecuritydemo;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
+import org.springframework.security.web.savedrequest.RequestCache;
+import org.springframework.security.web.savedrequest.SavedRequest;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @Configuration
 @EnableWebSecurity
@@ -27,16 +41,46 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        //인가API - 권한설정
-        //특정한 자원에 대해 보안 검사를 하고 싶을때 antMatchers()를 사용한다.
-        //단 구체적인 경로가 먼저오고 그것보다 큰 범위의 경로가 뒤에 오도록 해야 한다.
         http.authorizeRequests()
+                .antMatchers("/login").permitAll()
                 .antMatchers("/user").hasRole("USER")
                 .antMatchers("/admin/pay").hasRole("ADMIN")
                 .antMatchers("/admin/**").access("hasRole('ADMIN') or hasRole('SYS')")
                 .anyRequest().authenticated();
 
-        http.formLogin();
+        http.formLogin()
+                .successHandler(new AuthenticationSuccessHandler() {
+                    @Override
+                    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+                        RequestCache requestCache = new HttpSessionRequestCache();
+                        SavedRequest savedRequest = requestCache.getRequest(request, response);//원래 가고자 했던 요청정보가 저장됨. 이 객체를 얻어와서 다음 필터에 넘겨주는 필터는 RequestCacheAwareFilter이다.
+                        String redirectUrl = savedRequest.getRedirectUrl();
+                        response.sendRedirect(redirectUrl);
+                    }
+                });//인증에 성공한 유저가 원래 가고자 했던 곳으로 이동하게 함.
+
+        //FilterSecurityInterceptor가 요청을 받고 있음.
+        //user자원에 인증되지 않은 사용자가 접근하면 이 필터는 인증 예외를 발생시킴.
+        //더 정확하게는 인가예외로 감. 인증자체를 받으려는 시도를 안했기 때문. 즉 익명사용자가 접근하는 것이므로 인가예외로 빠짐.
+        //인가예외로 보냈을 때 AccessDeniedException에서 AccessDeniedHandler로 가지 않고 AuthenticationException으로 이동시켜서 로그인리다이렉트, 로그인시도 했던 사용자 요청정보를 캐싱한다.
+
+        //만약, 인증된 사용자지만 권한이 없는 사용자라면 인가 예외가 발생한다.
+        //이때는 AccessDeniedException에서 AccessDeniedHandler를 호출해서 후속 작업을 처리한다.
+
+        http.exceptionHandling()
+                .authenticationEntryPoint(new AuthenticationEntryPoint() {
+                    @Override
+                    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+                        response.sendRedirect("/login");//직접 만든 페이지로 이동하게 됨.
+                    }
+                })//인증 예외 시 처리
+                .accessDeniedHandler(new AccessDeniedHandler() {
+                    @Override
+                    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
+                        response.sendRedirect("/denied");
+                    }
+                });//인가 예외 시 처리
+
     }
 
 }
