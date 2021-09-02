c0dd3a815d6e6f43de32cc44156bf87ce47a881f
ljkk246 <ljkk246@gmail.com>
2021-09-03 00:03:44 +0900

    step02 : 02 : 다중 보안 설정 알아보기

--- src/main/java/com/example/springsecuritydemo/SecurityConfiguration.java
+++ src/main/java/com/example/springsecuritydemo/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package com.example.springsecuritydemo;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -25,21 +26,30 @@ import java.io.IOException;
 
 @Configuration
 @EnableWebSecurity
+@Order(0)//같은 설정클래스가 2개이므로 order로 순서를 구분지어야 한다. order 순서에 따라 시큐리티가 어느 설정 클래스를 먼저 검사하는 지 달라진다.
+//어느 설정 클래스를 먼저 검사하는지에 따라서 url matches 검사가 달라지므로, 넓은 범위의 url 검사하는 게 더 나중에 이루어져야 한다.
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-    @Autowired
-    UserDetailsService userDetailsService;
-
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.antMatcher("/admin/**")
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic();
+    }
+}
+@Order(1)
+@Configuration
+class SecurityConfiguration2 extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .anyRequest().permitAll();
-        http.formLogin();
-
-        //** DelegatingProxyChain, FilterChainProxy **
-        //springSecurityFilterChain의 이름으로 필터 리스트가 생성.
-        //DelegatingFilterProxy는 서블릿 필터의 요청을 위임받아 Spring bean에게 넘겨서 서블릿 필터가 스프링 기반으로 동작하게 하는데,
-        //DelegatingFilterProxy으로 부터 요청을 위임받고 실제 보안처릴를 하는 게 springSeucirtyFilterChain 목록에 있는 필터다.
-        //모든 필터에 대한 검사가 완료되면 DispatcherServelet으로 전달해서 실제 요청에 대한 처리를 진행.
+                .anyRequest().permitAll()
+                .and()
+                .formLogin();
+        //WebSecurity.java엘서 FIlterChainProxy 객체를 생성할 때 생성자 매개변수로 springFilterChains를 넣어주는데
+        //springFilterChains 에 config1,2에 대한 정보가 들어간다.
+        //사용자 요청에 따라 config1로 갈건지, config2로 갈건지 결정이 되는데. 이 결정하는 부분은 FIlterChainProxy.class에서 한다.
 
     }
 }
